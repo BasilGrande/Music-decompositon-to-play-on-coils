cf9b4f96757b90042673b5d5f9ac8b1e6019a666
Marvin Steinkellner <marvin.steinkellner@swissloop.ch>
2023-06-30 11:00:20 +0200

    [feature] improved ease of use of bin.py

--- bin.py
+++ bin.py
@@ -16,9 +16,17 @@ def sample_amplitude(filename, target_sampling_rate, duration):
     return audio_downsampled
 
 # Set the filename, desired sampling rate, and duration in seconds
-filename = "elise.mp3"
-target_sampling_rate = 20000  # 20kHz
-duration = 20  # Number of seconds to use from the song
+filename = input("enter the filename (incl .mp3): ")
+target_sampling_rate = int(float(input("enter the sampling frequency in kHz: "))*1000)
+if target_sampling_rate > 20000:
+    print("sampling rate too large -> target sampling rate set to 5kHz for safety")
+    target_sampling_rate = 5000
+
+duration = int(input("enter the duration in seconds: "))
+if(duration > 20 and target_sampling_rate > 5000):
+    print("duration may be too large --> set to 20s for safety")
+    duration = 20
+
 
 # Sample the amplitude
 amplitude_samples = sample_amplitude(filename, target_sampling_rate, duration)
@@ -42,7 +50,8 @@ rounded_samples = np.round(scaled_samples)
 rounded_samples = rounded_samples.astype(np.uint8)
 
 # Save the rounded amplitude samples to a binary file
-with open("20kHz_" + filename + ".bin", "wb") as f:
+prefix = str((int)(target_sampling_rate/1000)) + "kHz_"
+with open(prefix + filename[:-4] + ".grande", "wb") as f:
     for sample in rounded_samples:
         f.write(struct.pack('B', sample))
 
